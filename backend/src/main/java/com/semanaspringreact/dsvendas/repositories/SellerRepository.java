0c394a6854707dae8a8736209f194ccdc9186929
Maria Vitoria Magalhaes <mariavsmagalhaes@gmail.com>
2021-05-07 21:39:42 -0300

    Camadas

---
+++ backend/src/main/java/com/semanaspringreact/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.semanaspringreact.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.semanaspringreact.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
