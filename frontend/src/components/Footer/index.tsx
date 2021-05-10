921fe2363f0e611618fa7237893828c61fc02d6a
maria-vitoriaa <64209425+maria-vitoriaa@users.noreply.github.com>
2021-05-10 19:34:59 -0300

    Update index.tsx

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="linkedin.com/in/mariavitoriasilvamagalhaes" target="_blank" rel="noreferrer">Maria Vitória Magalhães</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://linkedin.com/in/mariavitoriasilvamagalhaes" target="_blank" rel="noreferrer">Maria Vitória Magalhães</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                         Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
